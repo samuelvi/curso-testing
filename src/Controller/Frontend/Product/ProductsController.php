6050ccd79a88b21ee2a4732bc0990db672621a3e
Samuel Vicent <samuel.vicent@takeachef.com>
2022-02-13 12:15:20 +0100

    wip: datafixtures

--- src/Controller/Frontend/Product/ProductsController.php
+++ src/Controller/Frontend/Product/ProductsController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SubscriptionController extends AbstractController
+class ProductsController extends AbstractController
 {
     /**
      * @Route("/products", name="products")
